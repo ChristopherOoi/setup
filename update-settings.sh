8dbbc2d084acaf32a38f0c979df172091ac47d09
ChristopherOoi <christopher_oo1@yahoo.com.sg>
2024-09-26 15:51:08 +0800

    updates

--- update-settings.sh
+++ update-settings.sh
@@ -3,8 +3,9 @@
 # copy updated .vimrc and .zshrc to this directory
 cp ~/.zshrc .
 cp ~/.vimrc .
+cp ~/.vim/coc-settings.json .
 
 # commit changes
-git add .zshrc .vimrc
+git add .zshrc .vimrc coc-settings.json
 git commit -m "Update settings"
 git push
